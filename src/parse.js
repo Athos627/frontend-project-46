3ed290ade626086f79458c714a76e90ec0cd91e7
Athos627 <kocher21@yandex.ru>
2025-10-11 18:45:46 +0300

    change parse.js

--- src/parse.js
+++ src/parse.js
@@ -1,12 +1,14 @@
 import fs from 'fs'
 import path from 'path'
-import parsed from './parser.js'
 
 const parsedFile = (filepath) => {
   const makePath = (fileName) => {
     return path.resolve(fileName)
   }
   const fileContent = fs.readFileSync(makePath(filepath), 'utf-8')
+  const parsed = (data) => {
+    return JSON.parse(data)
+  }
   return parsed(fileContent)
 }
 
