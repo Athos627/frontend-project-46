c69698b9d3766c321430e51b3453311e683731ff
Athos627 <kocher21@yandex.ru>
2025-10-17 16:05:21 +0300

    change in parse.js fuction makePath

--- src/parse.js
+++ src/parse.js
@@ -3,7 +3,11 @@ import path from 'path'
 
 const parsedFile = (filepath) => {
   const makePath = (fileName) => {
-    return path.resolve(fileName)
+    if (path.isAbsolute(fileName)) {
+      return path.resolve(fileName)
+    } else {
+      return path.resolve(process.cwd(), fileName)
+    }
   }
   const fileContent = fs.readFileSync(makePath(filepath), 'utf-8')
   const parsed = (data) => {
