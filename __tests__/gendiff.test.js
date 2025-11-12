0b733a916785bb8bcee675c7ae98409aa62df546
Athos627 <kocher21@yandex.ru>
2025-11-12 15:39:32 +0300

    add in test yaml format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,6 +2,10 @@ import getDiff from '../src/index.js'
 import fs from 'fs'
 import { test, expect } from '@jest/globals'
 
-test('getDiff test', () => {
+test('getDiff json test', () => {
   expect(getDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(fs.readFileSync('./__fixtures__/expectedResult.txt', 'utf-8'))
+})
+
+test('getDiff yml test', () => {
+  expect(getDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toBe(fs.readFileSync('./__fixtures__/expectedResult.txt', 'utf-8'))
 })
