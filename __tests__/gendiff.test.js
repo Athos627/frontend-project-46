566be20016af7557e93c7167553a8e274d359e62
Athos627 <kocher21@yandex.ru>
2025-12-09 21:10:45 +0300

    change tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,10 +2,26 @@ import getDiff from '../src/index.js'
 import fs from 'fs'
 import { test, expect } from '@jest/globals'
 
-test('getDiff json test', () => {
+test('getDiff json to stylish test', () => {
   expect(getDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(fs.readFileSync('./__fixtures__/expectedResult.txt', 'utf-8'))
 })
 
-test('getDiff yml test', () => {
+test('getDiff yml to stylish test', () => {
   expect(getDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toBe(fs.readFileSync('./__fixtures__/expectedResult.txt', 'utf-8'))
+})
+
+test('getDiff json to plain test', () => {
+  expect(getDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain')).toBe(fs.readFileSync('./__fixtures__/expectedPlain.txt', 'utf-8'))
+})
+
+test ('getDiff yaml to plain test', () => {
+  expect(getDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'plain')).toBe(fs.readFileSync('./__fixtures__/expectedPlain.txt', 'utf-8'))
+})
+
+test ('getDiff json to json test', () => {
+  expect(getDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json')).toBe(fs.readFileSync('./__fixtures__/expectedJson.txt', 'utf-8'))
+})
+
+test ('getDiff yaml to json test', () => {
+  expect(getDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'json')).toBe(fs.readFileSync('./__fixtures__/expectedJson.txt', 'utf-8'))
 })
